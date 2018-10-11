109b1f1c037045da8df28e2e6daad407f966dc80
Keilor Jimenez Quesada <keilorjimenez95@gmail.com>
2018-10-10 20:25:46 -0600

    Se crea clase student

---
+++ Students.php
@@ -0,0 +1,19 @@
+<?php
+class Student {
+  public $id;
+  public $first_name;
+  public $last_name;
+  public $email_address;
+  function Student($id, $first_name, $last_name, $email_address) {
+    $this->id = $id;
+    $this->first_name = $first_name;
+    $this->last_name = $last_name;
+    $this->email_address = $email_address;
+  }
+  
+  function to_string() {
+     return "{$this->id} - {$this->first_name} - {$this->last_name} - {$this->email_address} ". PHP_EOL;
+  }
+  
+}
+?>
